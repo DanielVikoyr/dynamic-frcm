486e7bc8ccc1a51b5834835016d51560f12f5dcd
AlexBringh <115021574+AlexBringh@users.noreply.github.com>
2024-02-13 19:31:26 +0100

    refactored RestAPI module to submodule of Weatherdata

---
+++ src/frcm/weatherdata/restapi/utils.py
@@ -0,0 +1,10 @@
+from frcm.datamodel.model import Location
+
+def parse_location ():
+    pass
+
+
+"""
+    Currently the functions that parse data being sent to and received from external Geocoding APIs live here. 
+    However if there becomes many functions and the code becomes a mess, they may be moved to their own separate files.
+"""
